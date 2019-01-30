e326b13d80d6ebf112212391be0b054d8ddc0474
Peter Ullrich <peter.j.ullrich@gmail.com>
2019-01-30 12:27:48 +0100

    Finished first working version of the crawler

--- crawler_new.py
+++ crawler_new.py
@@ -1,55 +1,74 @@
+import re
 from collections import deque
-from typing import List, Iterator
+from typing import Iterator
 from urllib.parse import urljoin, urlparse
 
+import progress
 import requests
 from bs4 import BeautifulSoup
+from progress.counter import Counter
+from requests import RequestException, Response
 
 
 class Crawler:
     root: str
     netloc: str
+    counter: progress.counter.Counter
 
     def __init__(self, root: str):
         self.root = root
-        self.netloc = urlparse(root).netloc
+        self.counter = Counter()
+        netloc_raw = urlparse(root).netloc
+        self.netloc = netloc_raw.replace('www.', '')
 
     def start(self):
-        broken = {}
         visited = set([self.root])
-        q = deque([self.root])
+        q = deque([(self.root, self.root)])
 
         while q:
-            url = q.popleft()
-            res = requests.get(url)
-
-            if res.status_code != requests.codes.ok:
-                broken[url] = res.status_code
-                print(f'Broken: {url}, States: {res.status_code}')
+            url, parent = q.popleft()
+            try:
+                res = requests.get(url)
+            except RequestException as e:
+                print(f'Error: {e} on {url}')
                 continue
 
-            for link in self._find_links(res.content):
-                if link not in visited and self._within_netloc(link):
-                    print(link)
-                    visited.add(link)
-                    q.append(link)
+            if res.status_code != requests.codes.ok:
+                print(f'Parent: {parent} - '
+                      f'Link: {url} '
+                      f'State: {res.status_code}')
+                continue
 
-    def _find_links(self, page: bytes) -> Iterator[str]:
-        soup = BeautifulSoup(page, 'html.parser')
-        links = [self._fix_link(a) for a in soup.find_all('a')]
+            for link in self._find_links(res):
+                if link not in visited:
+                    visited.add(link)
+                    q.append((link, url))
+
+                    self.counter.index += 1
+                    self.counter.update()
+
+    def _find_links(self, res: Response) -> Iterator[str]:
+        soup = BeautifulSoup(
+            res.content,
+            'html.parser',
+            from_encoding="iso-8859-1"
+        )
+        links = [self._format(res.url, a) for a in soup.find_all('a')]
         return filter(lambda l: l is not None, links)
 
-    def _fix_link(self, tag: {}):
+    def _format(self, url, tag: {}):
         link = tag.get('href', None)
-
         if link is None:
             return None
 
         parsed = urlparse(link)
         if parsed.netloc == '':
-            link = urljoin(self.root, link)
+            parent = urlparse(url)
+            netloc = parent.scheme + '://' + parent.netloc
+        else:
+            netloc = parsed.scheme + '://' + parsed.netloc
 
-        return link
+        if re.match('(.*).' + self.netloc, netloc) is None:
+            return None
 
-    def _within_netloc(self, link: str):
-        return urlparse(link).netloc == self.netloc
+        return urljoin(netloc, parsed.path)
